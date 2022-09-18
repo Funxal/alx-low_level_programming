e176c7aa03e673bb68a3ba346f22ac575a35e936
habibah <habibah049@gmail.com>
2022-09-18 00:19:47 +0000

    quick fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
+#include <string.h>
 #include <unistd.h>
 
 /**
- *  * main - Prints output
- *   *
- *    * Return: Always 0
- *     */
+ * * main - Prints out the last part of a quote in the standard error.
+ * * Return: 1 if success.
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	char buf1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, buf1, strlen(buf1));
+	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *    * This program prints: Programming is like building a multilingual puzzle
  *     *
  *      * Return: Always 0.
- *       */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
