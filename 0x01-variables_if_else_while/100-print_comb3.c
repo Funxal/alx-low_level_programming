4528e17cd6c52292ce2c786589d8ee4cecee7a2c
habibah <habibah049@gmail.com>
2022-10-08 22:31:59 +0000

    really hard

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,18 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
 	int e, h;
 	int i = 32;
 	int v = 44;
- 
+
 	h = 48;
 	e = 48;
 	while (e < 57)
 	{
 	h = 48;
-	while(h < 58)
+	while (h < 58)
 	{
 	if (h != e && e < h)
 	{
