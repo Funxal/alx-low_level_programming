8d5f0c858c1a7fe39d2a48457272da535c019b4a
habibah <habibah049@gmail.com>
2022-10-31 22:51:40 +0000

    hard fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * times_table - Print the 9 times table, starting with 0
  * Return: 0
  */
 void times_table(void)
 {
-		
 	int x = 0; /* factor */
 	int y; /* count  */
 	int z; /* computed value */
